704959b3875a85e3bd18b97e456c0a9f563a2f10
Portalbot1020 <portalbot1020@gmail.com>
2025-08-07 16:36:34 -0400

    Implement actual space physics
    applies physics to spaceship using rigidbody and central forces

--- asteroids/scripts/player.gd
+++ asteroids/scripts/player.gd
@@ -1,30 +1,16 @@
-extends CharacterBody2D
+extends RigidBody2D
 
+var thrust = Vector2(0, -250)
+var torque = 3000
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontaldirection := Input.get_axis("ui_left", "ui_right")
-	if horizontaldirection:
-		velocity.x = horizontaldirection * SPEED
+func _integrate_forces(state):
+	if Input.is_action_pressed("ui_up"):
+		state.apply_central_force(thrust.rotated(rotation))
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var verticaldirection := Input.get_axis("ui_up", "ui_down")
-	if verticaldirection:
-		velocity.y = verticaldirection * SPEED
-	else:
-		velocity.y = move_toward(velocity.y , 0, SPEED)
-
-	move_and_slide()
-	
+		state.apply_force(Vector2())
+	var rotation_direction = 0
+	if Input.is_action_pressed("ui_right"):
+		rotation_direction += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_direction -= 1
+	state.apply_torque(rotation_direction * torque)
